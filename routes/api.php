7a06297e9e489b991de3d7bd5b3442e25a13b63a
Omontor <omontor@gmail.com>
2022-02-01 11:52:08 -0600

    passport login

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\Api\BlogController;
+
+
+//Rutas de auth
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+
+//Rutas abiertas a todo el mundo
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('blog', [BlogController::class, 'index']);
+});
+
+
+
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
 });
