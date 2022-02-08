e9c59229f696e39548506746d6e7282508c1b9eb
TamaraPimentel <79122846+TamaraPimentel@users.noreply.github.com>
2022-02-08 12:51:25 -0600

    Dashboard
    Pills information
    Contact Form latest 5

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,11 +1,28 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+use App\Models\User;
+use App\Models\Transaction;
+use App\Models\Partner;
+use App\Models\Coupon;
+use App\Models\DynamicCoupon;
+use App\Models\QrCode;
+use App\Models\Analytic;
+use App\Models\ContactForm;
+
 
 class HomeController
 {
     public function index()
     {
-        return view('home');
+        $users = User::count();
+        $transactions = Transaction::count();
+        $partners = Partner::count();
+        $coupons = Coupon::count();
+        $dy_coupons = DynamicCoupon::count();
+        $qrs = QrCode::count();
+        $bryghia = Analytic::where('name', 'issued_bryghia')->get()->sum('value');
+        $contactForm = ContactForm::latest()->take(5)->get();
+        return view('home' ,compact('users','transactions','partners','coupons','dy_coupons','qrs','bryghia','contactForm'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,108 @@
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
+ 
                     @endif
+{{--Generals--}}
+    <div class="row">
+        <div class="col-lg-4 col-12">
+            <!-- small box -->
+            <div class="small-box bg-primary">
+              <div class="inner">
+                <h3>{{$users}}</h3>
+                <p>Total Users</p>
+              </div>
+            </div>
+        </div>
 
-                    You are logged in!
+        <div class="col-lg-4 col-12">
+            <!-- small box -->
+            <div class="small-box bg-secondary">
+              <div class="inner">
+                <h3>{{$transactions}}</h3>
+                <p>Total Transactions</p>
+              </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-12">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>{{$partners}}</h3>
+                <p>Total Partners</p>
+              </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-12">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{$coupons}}</h3>
+                <p>Coupons</p>
+              </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-12">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>{{$dy_coupons}}</h3>
+                <p>Dynamic Coupons</p>
+              </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-12">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{$qrs}}</h3>
+                <p>QRs</p>
+              </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-12">
+            <!-- small box -->
+            <div class="small-box bg-light">
+              <div class="inner">
+                <h3>{{$bryghia}}</h3>
+                <p>Total Issued Bryghias</p>
+              </div>
+            </div>
+        </div>    
+    </div>
+<hr>
+{{--ContactForms--}}  
+<h3>Latest Contact Forms</h3>
+    <table class="table table-striped table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Subject</th>
+          <th scope="col">Email</th>
+          <th scope="col">Message</th>
+        </tr>
+      </thead>
+        @forelse($contactForm as $form)
+      <tbody>
+        <tr>
+          <td>{{$form->name}}</td>
+          <td>{{$form->subject}}</td>
+          <td>{{$form->email}}</td>
+          <td>{{$form->message}}</td>
+        </tr>
+      </tbody>
+        @empty
+         There aren't any Messages yet.
+        @endforelse
+    </table>   
+
+{{--UsersXmonth--}}    
+
                 </div>
             </div>
         </div>
