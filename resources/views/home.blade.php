8899c37b199a81b9b50436cc4f9ccfb82a46ceed
TamaraPimentel <79122846+TamaraPimentel@users.noreply.github.com>
2022-02-08 18:30:44 -0600

    users_algo
    informacion de usuario semanal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -112,8 +112,404 @@
          There aren't any Messages yet.
         @endforelse
     </table>   
+<hr>
+<h3>Monthly Graphs</h3>
 
 {{--UsersXmonth--}}    
+<br>
+    <div class="row">
+        <h5>Users</h5>
+        <canvas id="usersChart" width="400" height="100"></canvas>
+
+        <script>
+        const user = document.getElementById('usersChart');
+        const usersChart = new Chart(user, {
+            type: 'line',
+            data: {
+                labels: [      
+                    @for ($i = 0; $i < 30; $i++)
+                    {{Carbon\Carbon::today()->subDays(30-$i)->format('d')}},
+                    @endfor
+                    {{Carbon\Carbon::today()->format('d')}},
+                    ],
+                datasets: [{
+                    label: 'New users in the latest 30 days',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 30; $i++)
+
+                {{DB::table('users')->whereDate('created_at', '=', now()->subDays(30-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('users')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                 fill: false,
+                    borderColor: '#0960e3',
+                    tension: 0.1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+        </script>  
+    </div>
+{{--TransactionsXmonth--}}  
+<br>
+    <div class="row">
+        <h5>Transactions</h5>
+        <canvas id="transacitonChart" width="400" height="100"></canvas>
+
+        <script>
+        const transaction = document.getElementById('transacitonChart');
+        const transacitonChart = new Chart(transaction, {
+            type: 'line',
+            data: {
+                labels: [      
+                    @for ($i = 0; $i < 30; $i++)
+                    {{Carbon\Carbon::today()->subDays(30-$i)->format('d')}},
+                    @endfor
+                    {{Carbon\Carbon::today()->format('d')}},
+                    ],
+                datasets: [{
+                    label: 'All transactions in the latest 30 days',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 30; $i++)
+
+                {{DB::table('transactions')->whereDate('created_at', '=', now()->subDays(30-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('transactions')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                 fill: false,
+                    borderColor: '#10ed05',
+                    tension: 0.1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+        </script>  
+    </div> 
+{{--PartnersXmonth--}}  
+<br>
+    <div class="row">
+        <h5>Partners</h5>
+        <canvas id="partnerChart" width="400" height="100"></canvas>
+
+        <script>
+        const partner = document.getElementById('partnerChart');
+        const partnerChart = new Chart(partner, {
+            type: 'line',
+            data: {
+                labels: [      
+                    @for ($i = 0; $i < 30; $i++)
+                    {{Carbon\Carbon::today()->subDays(30-$i)->format('d')}},
+                    @endfor
+                    {{Carbon\Carbon::today()->format('d')}},
+                    ],
+                datasets: [{
+                    label: 'New partners in the latest 30 days',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 30; $i++)
+
+                {{DB::table('partners')->whereDate('created_at', '=', now()->subDays(30-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('partners')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                 fill: false,
+                    borderColor: '#9c08d1',
+                    tension: 0.1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+        </script>  
+    </div>  
+{{--CouponsXmonth--}}  
+<br>
+    <div class="row">
+        <h5>Coupons</h5>
+        <canvas id="couponChart" width="400" height="100"></canvas>
+
+        <script>
+        const coupon = document.getElementById('couponChart');
+        const couponChart = new Chart(coupon, {
+            type: 'line',
+            data: {
+                labels: [      
+                    @for ($i = 0; $i < 30; $i++)
+                    {{Carbon\Carbon::today()->subDays(30-$i)->format('d')}},
+                    @endfor
+                    {{Carbon\Carbon::today()->format('d')}},
+                    ],
+                datasets: [{
+                    label: 'Coupons generate in the latest 30 days',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 30; $i++)
+
+                {{DB::table('coupons')->whereDate('created_at', '=', now()->subDays(30-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('coupons')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                 fill: false,
+                    borderColor: '#d108a2',
+                    tension: 0.1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+        </script>  
+    </div> 
+{{--QrXmonth--}}  
+<br>
+    <div class="row">
+        <h5>QRs</h5>
+        <canvas id="qrChart" width="400" height="100"></canvas>
+
+        <script>
+        const qr = document.getElementById('qrChart');
+        const qrChart = new Chart(qr, {
+            type: 'line',
+            data: {
+                labels: [      
+                    @for ($i = 0; $i < 30; $i++)
+                    {{Carbon\Carbon::today()->subDays(30-$i)->format('d')}},
+                    @endfor
+                    {{Carbon\Carbon::today()->format('d')}},
+                    ],
+                datasets: [{
+                    label: 'QRs generate in the latest 30 days',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 30; $i++)
+
+                {{DB::table('qr_codes')->whereDate('created_at', '=', now()->subDays(30-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('qr_codes')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                 fill: false,
+                    borderColor: '#b5b3b5',
+                    tension: 0.1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+        </script>  
+    </div>  
+{{--BryghisXmonth--}}  
+<br>
+    <div class="row">
+        <h5>Bryghia</h5>
+        <canvas id="bryghiaChart" width="400" height="100"></canvas>
+
+        <script>
+        const bryghia = document.getElementById('bryghiaChart');
+        const bryghiaChart = new Chart(bryghia, {
+            type: 'line',
+            data: {
+                labels: [      
+                    @for ($i = 0; $i < 30; $i++)
+                    {{Carbon\Carbon::today()->subDays(30-$i)->format('d')}},
+                    @endfor
+                    {{Carbon\Carbon::today()->format('d')}},
+                    ],
+                datasets: [{
+                    label: 'Bryghia generate in the latest 30 days',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 30; $i++)
+
+                {{DB::table('analytics')->where('name','issued_bryghia')->whereDate('created_at', '=', now()->subDays(30-$i)->format('Y-m-d'))->get()->sum('value')}},
+                    @endfor
+                    {{DB::table('analytics')->where('name','issued_bryghia')->whereDate('created_at', '=', now()->format('Y-m-d'))->get()->sum('value')}},
+                     ],
+
+                 fill: false,
+                    borderColor: '#fc9c00',
+                    tension: 0.1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+        </script>  
+    </div>        
+<hr>
+<h3>Users Graphs</h3>
+<br>
+
+{{--UsersInfo--}}    
+<br>
+    <div class="row">
+        <h5>Users Info</h5>
+        <canvas id="usersInfoChart" width="400" height="100"></canvas>
+
+        <script>
+        const user_info = document.getElementById('usersInfoChart');
+        const usersInfoChart = new Chart(user_info, {
+             type: 'bar',
+            data: {
+                labels: [      
+                    @for ($i = 0; $i < 7; $i++)
+                    {{Carbon\Carbon::today()->subDays(7-$i)->format('d')}},
+                    @endfor
+                    {{Carbon\Carbon::today()->format('d')}},
+                    ],
+                datasets: [{
+                    label: 'Unlocked Characters',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 7; $i++)
+
+                    {{DB::table('user_characters')->whereDate('created_at', '=', now()->subDays(7-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('user_characters')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                     fill: false,
+                    backgroundColor: '#05a86a',
+
+                        },
+                    {
+                    label: 'Unlocked Levels',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 7; $i++)
+
+                    {{DB::table('user_levels')->whereDate('created_at', '=', now()->subDays(7-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('user_levels')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                     fill: false,
+                    backgroundColor: '#0598a8',
+
+                        },
+                    {    
+                    label: 'Unlocked Landmarks',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 7; $i++)
+
+                    {{DB::table('user_landmarks')->whereDate('created_at', '=', now()->subDays(7-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('user_landmarks')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                     fill: false,
+                    backgroundColor: '#7705a8',
+
+                        }, 
+                    {    
+                    label: 'Generated QRs',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 7; $i++)
+
+                    {{DB::table('user_qr_codes')->whereDate('created_at', '=', now()->subDays(7-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('user_qr_codes')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                     fill: false,
+                    backgroundColor: '#a8057d',
+
+                        }, 
+                    {    
+                    label: 'Level Objects',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 7; $i++)
+
+                    {{DB::table('user_level_objects')->whereDate('created_at', '=', now()->subDays(7-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('user_level_objects')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                     fill: false,
+                    backgroundColor: '#a5a805',
+
+                        }, 
+                    {    
+                    label: 'Generated Questions',
+                    data:
+
+                     [
+                    @for ($i = 0; $i < 7; $i++)
+
+                    {{DB::table('user_level_questions')->whereDate('created_at', '=', now()->subDays(7-$i)->format('Y-m-d'))->count()}},
+                    @endfor
+                    {{DB::table('user_level_questions')->whereDate('created_at', '=', now()->format('Y-m-d'))->count()}},
+                     ],
+
+                     fill: false,
+                    backgroundColor: '#a83305',
+
+                        },           
+
+
+                        ]},
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+        </script>  
+    </div>
+
 
                 </div>
             </div>
