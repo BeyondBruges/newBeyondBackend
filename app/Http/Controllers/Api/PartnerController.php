70c71e498859001696b13f57331dce86f9848cd6
Omontor <omontor@gmail.com>
2022-02-28 19:23:48 -0600

    Update PartnerController.php

--- app/Http/Controllers/Api/PartnerController.php
+++ app/Http/Controllers/Api/PartnerController.php
@@ -10,7 +10,7 @@ class PartnerController extends Controller
 {
     public function index()
     {
-        $partners=Partner::with('partnerPartnerDescriptions');
+        $partners=Partner::with('partnerPartnerDescriptions')->get();
         return response()->json(['data'=>$partners], 200);
     }
 }
