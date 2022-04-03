fde8ca29677739acd6491099bb7084b6e81d273f
Omontor <omontor@gmail.com>
2022-04-03 15:47:57 -0500

    Update create.blade.php

--- resources/views/admin/transactions/create.blade.php
+++ resources/views/admin/transactions/create.blade.php
@@ -30,7 +30,7 @@
                 <span class="help-block">{{ trans('cruds.transaction.fields.value_helper') }}</span>
             </div>
             <div class="form-group">
-                <label class="required" for="status">{{ trans('cruds.transaction.fields.status') }}</label>
+                
                 <input class="form-control {{ $errors->has('status') ? 'is-invalid' : '' }}" type="number" name="status" id="status" value="1" step="1" hidden>
                 @if($errors->has('status'))
                     <span class="text-danger">{{ $errors->first('status') }}</span>
@@ -38,8 +38,8 @@
                 <span class="help-block">{{ trans('cruds.transaction.fields.status_helper') }}</span>
             </div>
 
-                        <div class="form-group">
-                <label class="required" for="transaction_type">{{ trans('cruds.transaction.fields.user') }}</label>
+            <div class="form-group">
+                Transaction Type
                 <select class="form-control select2 {{ $errors->has('user') ? 'is-invalid' : '' }}" name="transaction_type" id="transaction_type" required>
                     
                 <option value="1" {{ old('transaction_type') == $id ? 'selected' : '' }}>Bryghia Update</option>
