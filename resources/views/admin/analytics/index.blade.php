c43f65e53ad014f01f2b12a3015d9798a8e992df
Omontor <omontor@gmail.com>
2022-05-23 21:56:32 -0500

    Update index.blade.php

--- resources/views/admin/analytics/index.blade.php
+++ resources/views/admin/analytics/index.blade.php
@@ -69,19 +69,6 @@
                                     </a>
                                 @endcan
 
-                                @can('analytic_edit')
-                                    <a class="btn btn-xs btn-info" href="{{ route('admin.analytics.edit', $analytic->id) }}">
-                                        {{ trans('global.edit') }}
-                                    </a>
-                                @endcan
-
-                                @can('analytic_delete')
-                                    <form action="{{ route('admin.analytics.destroy', $analytic->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                    </form>
-                                @endcan
 
                             </td>
 
