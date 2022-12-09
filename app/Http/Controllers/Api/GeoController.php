df43a092e935aeb716c39162a9282b8b527c9e02
Omontor <omontor@gmail.com>
2022-12-09 11:15:19 -0500

    radius

--- app/Http/Controllers/Api/GeoController.php
+++ app/Http/Controllers/Api/GeoController.php
@@ -11,27 +11,27 @@ class GeoController extends Controller
 {
     public function calculate(Request $request){
 
-/*
- * @param1 : pass current latitude of the driver
- * @param2 : pass current longitude of the driver
- * @param3: pass the radius in meter within how much distance you wanted to fiter
-*/
-    $radius = 10;
-    $venues = Company::selectRaw("id, name, lat, lng,
-                     ( 6371 * acos( cos( radians(?) ) *
-                       cos( radians( lat ) )
-                       * cos( radians( lng ) - radians(?)
-                       ) + sin( radians(?) ) *
-                       sin( radians( lat ) ) )
-                     ) AS distance", [$request->lat, $request->lng, $request->lat])
-        ->having("distance", "<", $radius)
-        ->orderBy("distance",'asc')
-        ->offset(0)
-        ->limit(20)
-        ->get();
+        $radius = 19113;
+        /*
+
+        * replace 6371000 with 6371 for kilometer and 3956 for miles
+        */
+
+           $venues = Venue::selectRaw("id, name, address, lat, lng,
+                            ( 6371000 * acos( cos( radians(?) ) *
+                              cos( radians( lat ) )
+                              * cos( radians( lng ) - radians(?)
+                              ) + sin( radians(?) ) *
+                              sin( radians( lat ) ) )
+                            ) AS distance", [$request->lat, $request->lng, $request->lat])
+               ->having("distance", "<", $radius)
+               ->orderBy("distance",'asc')
+               ->offset(0)
+               ->limit(20)
+               ->get();
 
 
-    return response()->json(['data' => $venues], 200);
+           return response()->json(['data' => $venues], 200);
 
 
     }
