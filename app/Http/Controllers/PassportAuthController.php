a1562f661621c380cbe027a8ab4a1c63a127fb0c
Oliver Montor <omontor@gmail.com>
2023-03-20 23:21:03 -0400

    Update PassportAuthController.php

--- app/Http/Controllers/PassportAuthController.php
+++ app/Http/Controllers/PassportAuthController.php
@@ -58,12 +58,8 @@ class PassportAuthController extends Controller
      */
     public function login(Request $request)
     {
-        $data = [
-            'email' => $request->email,
-            'password' => $request->password
-        ];
-
         Log::debug($request);
+
 
         if ($request->password == "appleuser" || Str::contains($request->email, '@apple.com')){
 
@@ -77,6 +73,13 @@ class PassportAuthController extends Controller
                 return response()->json(['token' => $token], 200);
             }
         }
+
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+
 
         if (auth()->attempt($data)) {
             $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
