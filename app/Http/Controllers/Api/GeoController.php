58b45a59468341af9de70a7e905168fb325c2083
Oliver Montor <omontor@gmail.com>
2023-12-04 13:40:50 -0500

    BEYOND-1060   Fix Radius of geolocation

--- app/Http/Controllers/Api/GeoController.php
+++ app/Http/Controllers/Api/GeoController.php
@@ -11,24 +11,25 @@ class GeoController extends Controller
 {
     public function calculate(Request $request){
 
-        $radius = 19113;
+        $company = Company::first();
+        $radius = $company->radius != null ? $company->radius : 1;
+
         /*
-
-        * replace 6371000 with 6371 for kilometer and 3956 for miles
-        */
-
-           $venues = Company::selectRaw("id, name, lat, lng,
-                            ( 6371000 * acos( cos( radians(?) ) *
-                              cos( radians( lat ) )
-                              * cos( radians( lng ) - radians(?)
-                              ) + sin( radians(?) ) *
-                              sin( radians( lat ) ) )
-                            ) AS distance", [$request->lat, $request->lng, $request->lat])
-               ->having("distance", "<", $radius)
-               ->orderBy("distance",'asc')
-               ->offset(0)
-               ->limit(20)
-               ->get();
+         * using eloquent approach, make sure to replace the "Restaurant" with your actual model name
+         * replace 6371000 with 6371 for kilometer and 3956 for miles
+         */
+        $venues = Company::selectRaw("id, name, lat, lng,
+                         ( 6371 * acos( cos( radians(?) ) *
+                           cos( radians( lat ) )
+                           * cos( radians( lng ) - radians(?)
+                           ) + sin( radians(?) ) *
+                           sin( radians( lat ) ) )
+                         ) AS distance", [$request->lat, $request->lng, $request->lat])
+            ->having("distance", "<", $radius)
+            ->orderBy("distance",'asc')
+            ->offset(0)
+            ->limit(20)
+            ->get();
 
 
            return response()->json(['data' => $venues], 200);
