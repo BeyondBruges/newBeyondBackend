1294018371ddedbf9bd149de48da4885dadaa897
Oliver Montor <omontor@gmail.com>
2024-04-10 19:44:06 -0400

    Update TransactionController.php

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -54,7 +54,7 @@ class TransactionController extends Controller
                     $userId = $user->udid;
 
                     OneSignal::sendNotificationToUser(
-                       '{"en":'.str_replace($searchVal, $replaceVal, $messageLoc->$content).'}',
+                        str_replace($searchVal, $replaceVal, $messageLoc->$content),
                         $userId,
                         $url = null,
                         $data = null,
